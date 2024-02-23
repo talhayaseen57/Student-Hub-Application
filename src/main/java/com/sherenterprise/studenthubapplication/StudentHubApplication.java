b9a5f7f7da9e7833cb16f26862be5a5f981cda61
talhayaseen57 <talhayaseen5725@gmail.com>
2024-02-23 14:45:54 +0100

    First Commit

---
+++ src/main/java/com/sherenterprise/studenthubapplication/StudentHubApplication.java
@@ -0,0 +1,13 @@
+package com.sherenterprise.studenthubapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sherenterprise/studenthubapplication/StudentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherenterprise.studenthubapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
