fa1ea1a5d2eb6b1756f2d1c832a90b72ef119ac1
talhayaseen57 <talhayaseen5725@gmail.com>
2024-02-23 15:33:21 +0100

    Added users to InMemoryUserDetailsManager

--- src/main/java/com/sherenterprise/studenthubapplication/security/SecurityConfig.java
+++ src/main/java/com/sherenterprise/studenthubapplication/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.sherenterprise.studenthubapplication.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,10 +24,27 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(HttpMethod.GET).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails student01 = User.builder()
+                .username("student01")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, student01);
     }
 
 }
