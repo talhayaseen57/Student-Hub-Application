b92c35a9260a70326f5ef98fb0eedeefc4b3128e
talhayaseen57 <talhayaseen5725@gmail.com>
2024-02-23 15:42:16 +0100

    Added Authority entity

--- src/main/java/com/sherenterprise/studenthubapplication/domain/UserEntity.java
+++ src/main/java/com/sherenterprise/studenthubapplication/domain/UserEntity.java
@@ -3,6 +3,8 @@ package com.sherenterprise.studenthubapplication.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -15,6 +17,10 @@ public class UserEntity {
     private LocalDate cohortStartData;
     private String username;
     private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -40,5 +46,10 @@ public class UserEntity {
     public void setPassword(String password) {
         this.password = password;
     }
-
+    public List<Authority> getAuthorities() {
+        return authorities;
+    }
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
 }
