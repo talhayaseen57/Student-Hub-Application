f8eff0fdb6295231a0890d5d20bd88586ff04739
talhayaseen57 <talhayaseen5725@gmail.com>
2024-02-24 23:49:28 +0100

    Created repository interfaces

---
+++ src/main/java/com/sherenterprise/studenthubapplication/repository/AuthorityRepository.java
@@ -0,0 +1,12 @@
+package com.sherenterprise.studenthubapplication.repository;
+
+import com.sherenterprise.studenthubapplication.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+
+    Optional<Authority> findByName(String name);
+
+}
