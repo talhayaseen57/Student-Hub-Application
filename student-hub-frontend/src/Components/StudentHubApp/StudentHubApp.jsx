4a14da6720a8eb38cddd0f14cc6e546d0d5c63bf
talhayaseen57 <talhayaseen5725@gmail.com>
2024-03-20 21:26:08 +0100

    adding useEffect() hook
    to render the StudentHubApp.jsx once, useEffect() hook is used

--- student-hub-frontend/src/Components/StudentHubApp/StudentHubApp.jsx
+++ student-hub-frontend/src/Components/StudentHubApp/StudentHubApp.jsx
@@ -1,27 +1,36 @@
+import { useEffect, useState } from 'react';
 import './StudentHubApp.css'
 
 export const StudentHubApp = () => {
-    console.log("Hello World!");
 
-    const requestBody = {
-        'username': 'student02',
-        'password': '102'
-    }
+    const [jwtToken, setJwtToken] = useState("");
 
-    fetch('http://localhost:8080/auth/login', {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'post',
-        body: JSON.stringify(requestBody)
-    })
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+    useEffect(() => {
+        console.log("Hello World!");
+
+        const requestBody = {
+            'username': 'student02',
+            'password': '102'
+        }
+    
+
+        fetch('http://localhost:8080/auth/login', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'post',
+            body: JSON.stringify(requestBody)
+        })
+            .then((response) => response.json())
+            .then((data) => setJwtToken(data.accessToken));
+    }, []);
+    
 
 
     return (
         <div className='container'>
             <div>Student Hub Application</div>
+            <div>JWT Token is ${jwtToken}</div>
         </div>
     );
 
